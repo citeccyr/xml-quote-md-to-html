2b49d96cffce4758c8a26a2d4efa464ce973f4ea
Mrau Hu <mrauhu@yandex.ru>
2018-08-20 11:25:05 +0300

    Remove package `marked`, added package `markdown-it`. Fix `test/index.js`.

--- test/index.js
+++ test/index.js
@@ -1,7 +1,12 @@
 const assert = require('assert');
 const fs = require('fs');
 const transform = require('camaro');
-const marked = require('marked');
+const MarkdownIt = require('markdown-it');
+
+// Extensions enabled by default:
+// https://github.github.com/gfm/
+// https://github.github.com/gfm/#tables-extension
+const md = new MarkdownIt();
 
 const xml = fs.readFileSync('test/pannot3.xml', 'utf-8');
 
@@ -13,15 +18,10 @@ const template = {
 const data = transform(xml, template);
 //console.log('data', data);
 
-/** @type {string} */
-const html = marked(data.quote, {
-  // https://github.github.com/gfm/
-  gfm: true,
-  // https://github.github.com/gfm/#tables-extension
-  tables: true,
-});
-//console.log('html', html);
+const html = md.render(data.quote);
 
-const reference_html = '<p>Look at diagrams at <a href="http://tabbles.net/what-is-it/whats-tabbles/">http://tabbles.net/what-is-it/whats-tabbles/</a><br><img src="http://tabbles.net/images/stories/web_v2/new_pic_dragndrop.png" alt="like this one"></p>\n';
+const reference_html = `<p>Look at diagrams at <a href="http://tabbles.net/what-is-it/whats-tabbles/">http://tabbles.net/what-is-it/whats-tabbles/</a><br>
+<img src="http://tabbles.net/images/stories/web_v2/new_pic_dragndrop.png" alt="like this one"></p>
+`;
 
 assert.strictEqual(html, reference_html);
